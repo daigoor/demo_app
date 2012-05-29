f1666fcd62a8fbb16bc8bd5738829a298a5ff9a9
root <root@localhost.localdomain>
2012-05-29 06:47:03 -0700

    this is a test comment on committing gi repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f8186277d5534473d6bd5cab4330dee37e11010df09cfcd37f46b3a4a2a116fdb736d4ad5664dd95610072ae8f5f83581d0907928fd7bf8d409c3e175bdbb0af'
